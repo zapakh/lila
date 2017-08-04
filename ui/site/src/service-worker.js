e23c4a495cf2a1ce02ba67fd2d35df3f92b26577
Thibault Duplessis <thibault.duplessis@gmail.com>
2017-08-04 11:59:54 +0200

    homemade service worker WIP

---
+++ ui/site/src/service-worker.js
@@ -0,0 +1,35 @@
+function subscribeDevice() {
+  navigator.serviceWorker.ready.then(function (serviceWorkerRegistration) {
+    // Demande d'inscription au Push Server (1)
+    return serviceWorkerRegistration.pushManager.subscribe({ userVisibleOnly: true });
+  }).then(function (subscription) {
+    //sauvegarde de l'inscription dans le serveur applicatif (2)
+    fetch('/register-to-notification', {
+      method: 'post',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      credentials: 'same-origin',
+      body: JSON.stringify(subscription)
+    }).then(function (response) {
+      return response.json();
+    }).catch(function (err) {
+      console.log('Could not register subscription into app server', err);
+    });
+  }).catch(function (subscriptionErr) {
+    // Check for a permission prompt issue
+    console.log('Subscription failed ' + subscriptionErr);
+  });
+}
+
+if ('serviceWorker' in navigator) lichess.requestIdleCallback(function() {
+  // enregistrement du fichier 'service-worker.js' présent à la racine de l'application
+  navigator.serviceWorker.register('/sw.js').then(function(reg) {
+    console.log('Registration succeeded. Scope is ' + reg.scope);
+    subscribeDevice();
+  }).catch(function (error) {
+    // registration failed
+    console.log('Registration failed with ' + error);
+  });
+});
